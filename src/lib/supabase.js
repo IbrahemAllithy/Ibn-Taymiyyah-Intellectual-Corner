6fa0fdf9f5d59f6e6c6a75dda824d99da7ca6bbf
IbrahemAllithy <ibrahem.allithy@gmail.com>
2026-07-12 17:56:10 +0300

    Fix: correct typo in Supabase URL

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://mjrqirvtcrdzjijgghip.supabase.co';
+const supabaseUrl = 'https://mjrqirvtcrdjzijgghip.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1qcnFpcnZ0Y3JkanppamdnaGlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM4NTUwNTQsImV4cCI6MjA5OTQzMTA1NH0.SPoeIOzzPMbWusPmVu-MZxKScZTSsG_sljtljy4mnF8';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
